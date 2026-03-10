7519a9fd613aaceecce0cb2ec456ce3fb77c29ba
Ronhenley08121 <falsarioaaron@gmail.com>
2026-03-10 21:55:29 +0800

    student log in pagee

---
+++ LOGIN/function.js
@@ -0,0 +1,12 @@
+
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('passwordInput');
+
+    if (togglePassword && passwordInput) {
+        togglePassword.addEventListener('click', function() {
+            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordInput.setAttribute('type', type);
+            this.classList.toggle('fa-eye');
+            this.classList.toggle('fa-eye-slash');
+        });
+    }
