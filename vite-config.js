c0dd325e140861b74b5427870f50ae33afb8f363
Ronhenley08121 <falsarioaaron@gmail.com>
2026-05-05 16:15:32 +0800

    vite.config

--- vite-config.js
+++ vite-config.js
@@ -13,12 +13,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        admin: resolve(__dirname, 'Assets/Admin_dashboard/Admin.html'),
-        incidents: resolve(__dirname, 'Assets/Admin_dashboard/incident/incident.html'),
-        users: resolve(__dirname, 'Assets/Admin_dashboard/user_page/user.html'),
-        settings: resolve(__dirname, 'Assets/Admin_dashboard/settings/setting.html'),
-        login: resolve(__dirname, 'Assets/login/admin/admin.html'),
-        landing: resolve(__dirname, 'Assets/Landing_page/land.html')
+        admin: resolve(__dirname, '/Assets/Admin_dashboard/Admin.html'),
+        incidents: resolve(__dirname, '/Assets/Admin_dashboard/incident/incident.html'),
+        users: resolve(__dirname, '/Assets/Admin_dashboard/user_page/user.html'),
+        settings: resolve(__dirname, '/Assets/Admin_dashboard/settings/setting.html'),
+        login: resolve(__dirname, '/Assets/login/admin/admin.html'),
+        landing: resolve(__dirname, '/Assets/Landing_page/land.html')
       }
     },
     assetsInlineLimit: 0,
